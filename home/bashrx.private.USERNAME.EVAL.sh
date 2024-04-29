584ceeb7ee1320ec74fd51db424322198b5af836
Landon Bouma <landonb@users.noreply.github.com>
2024-04-29 16:23:15 -0500

    Improve: Add empty private Bashrc
    - Save user the trouble of creating.
    
    - Makes it more discoverable, because it'll show up in
      the project tray file list, and directory listings.

---
+++ home/bashrx.private.USERNAME.EVAL.sh
@@ -0,0 +1,32 @@
+# USAGE:
+#   m4_shim \
+#     "+10" "home/bashrx.private.USERNAME.EVAL.sh" \
+#     "home/bashrx.private.${DXY_USERNAME}.sh" \
+#     "BUSINESS_PROJECT_BASE" \
+#       "https://${BUSINESS_GITCONFIG_HUB_HOST}/$(dirname -- "${BUSINESS_GITSERVER_USER_REPO}")"
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# vim:tw=0:ts=2:sw=2:et:norl:ft=bash
+# DXY_HEADER_AUTHOR
+# DXY_HEADER_DOTPROJECT
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# USAGE: See the example file for an idea of what to add herein:
+#
+#   DXY_DEPOXY_CLIENT_TILDE/home/bashrx.private.DXY_USERNAME.EVAL.sh.example
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+main () {
+  # Wait for the second pass to run.
+  ${HOME_FRIES_PRELOAD} && return
+
+  # No-op.
+  :
+}
+
+main "$@"
+unset -f main
+
