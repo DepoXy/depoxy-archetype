c9b6e3b7a6341ade2ac2850a356ca749d68bcb4d
Landon Bouma <landonb@users.noreply.github.com>
2024-05-01 16:05:22 -0500

    Docs: Add comment

--- home/bashrx.private.HOSTNAME.EVAL.sh
+++ home/bashrx.private.HOSTNAME.EVAL.sh
@@ -157,6 +157,9 @@ bashdx_wire_aliases_open_tracker () {
 bashdx_customize_depoxy_client_PS1 () {
   . "${HOMEFRIES_LIB:-${HOME}/.homefries/lib}/term/set-shell-prompt-and-window-title.sh"
 
+  # USAGE: Disable this if you want to see the machine name in the prompt
+  # instead of the DXC Client ID.
+  # - ALTLY: Customize this name part or all of PS1 to your liking.
   HOMEFRIES_TERM_UTIL_PS1_HOST="DXC${DEPOXY_CLIENT_ID}" \
     dubs_set_terminal_prompt
 }
