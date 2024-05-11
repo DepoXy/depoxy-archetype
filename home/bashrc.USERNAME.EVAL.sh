9493c4eba0082b60b48b92741b0f8b3b3fcde05e
Landon Bouma <landonb@users.noreply.github.com>
2024-05-10 19:00:27 -0500

    Insert: Add private user Bashrc template

---
+++ home/bashrc.USERNAME.EVAL.sh
@@ -0,0 +1,21 @@
+# USAGE:
+#   m4_shim \
+#     "+8" "home/bashrc.USERNAME.EVAL.sh" \
+#     "home/bashrc.${DXY_USERNAME}.sh"
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# vim:tw=0:ts=2:sw=2:et:norl:ft=bash
+# DXY_HEADER_AUTHOR
+# DXY_HEADER_PROJECT
+# DXY_HEADER_LICENSE
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+main () {
+  :
+}
+
+main "$@"
+unset -f main
+
