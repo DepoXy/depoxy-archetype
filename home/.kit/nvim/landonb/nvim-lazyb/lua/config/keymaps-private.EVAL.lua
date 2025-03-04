901f28c1fa8c1b12bfff33db890211ab3cf0f17b
Landon Bouma <landonb@users.noreply.github.com>
2025-03-04 13:34:01 -0600

    feat(nvim): add private Neovim Lua configs
    These will be loaded by DepoXy Neovim Lua config, which will be
    loaded nvim-lazyb if they exist (or silently skipped if they don't).

---
+++ home/.kit/nvim/landonb/nvim-lazyb/lua/config/keymaps-private.EVAL.lua
@@ -0,0 +1,13 @@
+-- USAGE:
+--   m4_shim \
+--     "+8" "home/.kit/nvim/landonb/nvim-lazyb/lua/config/keymaps-private.EVAL.lua" \
+--     "home/.kit/nvim/landonb/nvim-lazyb/lua/config/keymaps-private.lua"
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+-- vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=lua
+-- DXY_HEADER_AUTHOR
+-- DXY_HEADER_PROJECT
+-- DXY_HEADER_LICENSE
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ --
---
+++ home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-private.EVAL.lua
@@ -0,0 +1,13 @@
+-- USAGE:
+--   m4_shim \
+--     "+8" "home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-private.EVAL.lua" \
+--     "home/.kit/nvim/landonb/nvim-lazyb/lua/config/options-private.lua"
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+-- vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=lua
+-- DXY_HEADER_AUTHOR
+-- DXY_HEADER_PROJECT
+-- DXY_HEADER_LICENSE
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ --
